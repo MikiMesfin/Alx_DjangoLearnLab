a7c171a0150ec75184139f37fc3744944accd6f3
MikiMesfin <mikimesfin45@yahoo.com>
2024-09-21 00:20:31 +0300

    add file

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,11 +6,11 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import permissions.IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .models import CustomUser
+
 
 # Create your views here.
 
