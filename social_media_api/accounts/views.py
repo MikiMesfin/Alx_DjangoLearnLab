d3437357ec9c56077f86a4221b03bab8ad41aedd
MikiMesfin <mikimesfin45@yahoo.com>
2024-09-20 23:38:51 +0300

    Add Dir

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,21 +4,16 @@ from .models import CustomUser
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from .serializers import UserSerializer
 
 # Create your views here.
-
- class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
-        user = CustomUser.objects.get(id=token.user_id)
+        user = token.user
         return Response({
             'token': token.key,
-            'user_id': user.pk,
-            'username': user.username
+            'user': UserSerializer(user).data  
         })
